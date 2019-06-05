32ebcca0b28c751b93c67894ef1e5a82451d3f6e
leekheang <ly.kheang.jr@gmail.com>
2019-06-05 09:51:41 +0700

    2 looplist

--- netscan.py
+++ netscan.py
@@ -1,13 +1,16 @@
-from scapy.all import ARP , Ether , ls
+#conda instal -c pdrop scapy
+#run as root
+import scapy.all as scapy
 
 def scan(ip):
-    arp_req = ARP(pdst = ip)
-    arp_req.show()
-    broadcast = Ether(dst="a0:af:bd:e5:44:0d")
-    broadcast.show()
+    arp_req = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_req_broadcast = broadcast / arp_req
-    arp_req_broadcast.show()
-    #ls(Ether())
-    # print(broadcast.summary()) 
+    answered_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
+    
+    for el in answered_list:
+        print(el[1].psrc)
+        print(el[1].hwsrc)
+        print("----------------------------------")
 
 scan("192.168.1.1/24")
