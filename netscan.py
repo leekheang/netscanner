f496380500661fe9046df18ffa21dbb9418a62de
leekheang <ly.kheang.jr@gmail.com>
2019-06-05 10:59:30 +0700

    4 createdict

--- netscan.py
+++ netscan.py
@@ -9,8 +9,12 @@ def scan(ip):
     answered_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
     
     print("IP\t\t\tMac Address\n-----------------------------------------")
+    client_list = []
     for el in answered_list:
+        client_dict = {"ip": el[1].psrc, "mac": el[1].hwsrc}
+        client_list.append(client_dict)
         print(el[1].psrc + "\t\t" + el[1].hwsrc)
         print("-----------------------------------------")
+    print(client_list)
 
 scan("192.168.1.1/24")
