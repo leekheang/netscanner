2c0eefc16ac483d48d33caf57ca0c6988e449c16
leekheang <ly.kheang.jr@gmail.com>
2019-06-06 10:29:41 +0700

    end netscan

--- netscan.py
+++ netscan.py
@@ -1,13 +1,28 @@
-#conda instal -c pdrop scapy
+#conda install -c pdrops scapy
 #run as root
-import scapy.all as scapy
+import scapy.all as  scapy
+import argparse 
+# import optparse 
+
+# def get_arguments():
+#         parser = optparse.OptionParser()
+#         parser.add_option("-t", "--target", dest="target", help="Target IP/ IP range .")
+#         (options , arguments) = parser.parse_args()
+#         return options
+
+
+def get_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-t", "--target", dest="target", help="Target IP/ IP range .")
+        options  = parser.parse_args()
+        return options
 
 def scan(ip):
     arp_req = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_req_broadcast = broadcast / arp_req
     answered_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
-    
+ 
     
     client_list = []
     for el in answered_list:
@@ -19,6 +34,6 @@ def print_result(result_list):
         print("IP\t\t\tMac Address\n-----------------------------------------")
         for client in result_list:
                 print(client["ip"] + "\t\t" + client["mac"])
-
-scan_result = scan("192.168.1.1/24")
+options = get_arguments()
+scan_result = scan(options.target)
 print_result(scan_result)
